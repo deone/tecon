40cb955092fb84ebfa83ed07d68d43a89d66e647
Oladayo Osikoya <alwaysdeone@gmail.com>
2009-12-16 10:14:05 +0100

    Logo in place. Remove 'home' link, logo is home link.

--- header.inc.php
+++ header.inc.php
@@ -1,13 +1,12 @@
     <div id="header">
 	<div class="wrap">
-	    <!--<a href="http://www.teconoilserv.com"><img id="logo" src="img/logo.gif"></a>-->
+	    <a href="http://www.teconoilserv.com"><img id="logo" src="img/logo.jpg"></a>
 	    <p id="mission">
 		Our mission is to be the preferred indigenous Oilfield Service Contractor providing cost effective / leading 
 		edge Equipment / Tools, Rental, and Integrated Well Engineering Services to our customers at high investment 
 		returns to our shareholders.
 	    </p>
 	    <ul>
-		<li><a href="index.php">Home</a></li>
 		<li><a href="about.php">About Us</a></li>
 		<!--<li><a href="index.php#content">Services</a></li>-->
 		<li><a href="clients.php">Clients</a></li>
