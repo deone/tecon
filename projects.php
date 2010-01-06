40664782295959486230dce3e9fa015bc0412b2d
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-01-06 08:02:23 +0100

    Changed body font type to Arial. Re-arrange project entries in projects.php.

--- projects.php
+++ projects.php
@@ -37,12 +37,6 @@
 			</thead>
 			<tbody>
 			    <tr>
-				<td>TotalFinaElf</td>
-				<td>Amenam/Kpono AK-DC-11, Provision of Downhole Tools Rentals and Fishing Services</td>
-				<td>July 2002 to July 2005</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
 				<td>Shell Nigeria Developement Company - S16602</td>
 				<td>Provision of Integrated Drilling Tools/Rental &amp; Services to Include: Casing/Tubing 
 					Running Equipment &amp; Casing Crew Solids Control Equipment Rental &amp; Services
@@ -53,10 +47,22 @@
 				<td>Ongoing</td>
 			    </tr>
 			    <tr>
-				<td>Shell Nigeria Contract S13069</td>
-				<td>Provision of Hydraulic Workover Units</td>
-				<td>May 2002 - 2005, Under 2nd extension till July 2007</td>
-				<td>Completed</td>
+				<td>NAOC</td>
+				<td>Supply of snubbing rig(ISS 460K Hydrastar II',) for workover intervention in NAOC land area</td>
+				<td>June 2009 - June 2010</td>
+				<td>Ongoing</td>
+			    </tr>
+			    <tr>
+				<td>Chevron Nig Ltd Ref. DRL-2005-40027904</td>
+				<td>Casing / Tubing Running Services</td>
+				<td>2005-2007, Ext till June 2010</td>
+				<td>Ongoing</td>
+			    </tr>
+			    <tr>
+				<td>ENI Congo Contract no. 8090000000/105</td>
+				<td>Provision of hydraulic workover for offshore/onshore workover operations.</td>
+				<td>Feb 2009 - till date</td>
+				<td>Ongoing</td>
 			    </tr>
 			    <tr>
 				<td>Addax Pet Explor. Nig Ltd, Contract ETB-0011</td>
@@ -77,15 +83,21 @@
 				<td>Ongoing</td>
 			    </tr>
 			    <tr>
-				<td>Addax Pet Dev Nig Ltd, Contract  TB-1894</td>
-				<td>Provision  of Fishing Services (on DWC 20)</td>
-				<td>April 2007 to April 2009 with the Option of One Year Extension</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
 				<td>Addax Pet Dev Nig Ltd, Contract  TB-1537</td>
 				<td>Provision  of Fishing &amp; Milling Services (on Noble Tommy Craighead)</td>
 				<td>August 2006 to August 2007 , Ext till April 2009, till date</td>
+				<td>Ongoing</td>
+			    </tr>
+			    <tr>
+				<td>Esso Exploration and Production Nigeria Ltd Agreement No. A2074187</td>
+				<td>Drill String &amp; Fishing Rental Tools Services</td>
+				<td>2007 - 2009 with the Option of One Year Extension</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
+				<td>Frontier Oil Ltd</td>
+				<td>Hire of Hydraulic Workover Unit [HWU] for Workover Ops Services.</td>
+				<td>Jan. 2008 to March 2008. Yet to commence.</td>
 				<td>Ongoing</td>
 			    </tr>
 			    <tr>
@@ -100,11 +112,24 @@
 				<td>June 2006 to June 2008 with the Option of One Year Extension</td>
 				<td>Ongoing</td>
 			    </tr>
+			    
 			    <tr>
-				<td>Chevron Nig Ltd Ref. DRL-2005-40027904</td>
-				<td>Casing / Tubing Running Services</td>
-				<td>2005-2007, Ext till June 2010</td>
+				<td>NAOC / Corporate Contract  Ref. N03Y0747P/00</td>
+				<td>Fishing Services for Drilling &amp; Completion</td>
+				<td>July 2003 - 2006, Extended till Dec 2009</td>
 				<td>Ongoing</td>
+			    </tr>
+			    <tr>
+				<td>Pioneer Natural Resources Nigeria (320) Limited Contract Ref. PNRN(320)L-DC-003</td>
+				<td>Drilling Equipment Rental Services</td>
+				<td>2007 -2010  Yet to commence</td>
+				<td>Ongoing</td>
+			    </tr>
+			    <tr>
+				<td>Addax Pet Dev Nig Ltd, Contract  TB-1894</td>
+				<td>Provision  of Fishing Services (on DWC 20)</td>
+				<td>April 2007 to April 2009 with the Option of One Year Extension</td>
+				<td>Completed</td>
 			    </tr>
 			    <tr>
 				<td>Schlumberger Supply Chain Services, Ref. 2005-04-15</td>
@@ -113,23 +138,24 @@
 				<td>Completed</td>
 			    </tr>
 			    <tr>
+				<td>TotalFinaElf</td>
+				<td>Amenam/Kpono AK-DC-11, Provision of Downhole Tools Rentals and Fishing Services</td>
+				<td>July 2002 to July 2005</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
+				<td>Shell Nigeria Contract S13069</td>
+				<td>Provision of Hydraulic Workover Units</td>
+				<td>May 2002 - 2005, Under 2nd extension till July 2007</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
 				<td>Elf Petroleum Ltd, Ref. NG00003050/DRI/04</td>
 				<td>Down-hole Tools &amp; Fishing Services	</td>
 				<td>2005-2007, Plus One Year Extension</td>
 				<td>Completed</td>
 			    </tr>
-			    <tr>
-				<td>NAOC / Corporate Contract  Ref. N03Y0747P/00</td>
-				<td>Fishing Services for Drilling &amp; Completion</td>
-				<td>July 2003 - 2006, Extended till Dec 2009</td>
-				<td>Ongoing</td>
-			    </tr>
-			    <tr>
-				<td>Esso Exploration and Production Nigeria Ltd Agreement No. A2074187</td>
-				<td>Drill String &amp; Fishing Rental Tools Services</td>
-				<td>2007 - 2009 with the Option of One Year Extension</td>
-				<td>Completed</td>
-			    </tr>
+			    
 			    <tr>
 				<td>Esso Exploration and Production Nigeria Ltd Agreement No. 18103152</td>
 				<td>Drilling Rental Tools &amp; Fishing</td>
@@ -137,10 +163,40 @@
 				<td>Completed</td>
 			    </tr>
 			    <tr>
-				<td>Pioneer Natural Resources Nigeria (320) Limited Contract Ref. PNRN(320)L-DC-003</td>
-				<td>Drilling Equipment Rental Services</td>
-				<td>2007 -2010  Yet to commence</td>
-				<td>Ongoing</td>
+				<td>CONOCO CEPNL-09-00</td>
+				<td>Provision of Rental Tools</td>
+				<td>2001-2002</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
+				<td>Stardeep Water Petroleum Ltd Contract LS00-009B</td>
+				<td>General Services Agreement for the supply of Downhole Equipment Rental and Rotating Mousehole Tool (RMT)TM and Twister Torque Tool (TT)TM used for breaking out and making up DCs, DPs.</td>
+				<td>2001-2002</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
+				<td>Halliburton Energy Services Contract Nig-SNEP-99-002</td>
+				<td>Rental &amp; Supply of Tubing Test String and Tubing Running Services </td>
+				<td>1999-2003</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
+				<td>Halliburton Energy Services Contract Ref. LOS-00-03-01</td>
+				<td>Integrated Completion Services Bonga Field</td>
+				<td>Aug 2002 - Aug 2005</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
+				<td>Nigeria Agip Exploration Limited Ref.230039/0</td>
+				<td>Provision of Down Hole Equipment Services</td>
+				<td>2004/2005</td>
+				<td>Completed</td>
+			    </tr>
+			    <tr>
+				<td>Independent Energy Ref: 20.M0012.00/11001</td>
+				<td>Hydraulic Workover Operations</td>
+				<td>June 2007 to Sept 2007</td>
+				<td>Completed</td>
 			    </tr>
 			    <tr>
 				<td>Shell Nigeria PH</td>
@@ -215,60 +271,7 @@ contractors) for Development Drilling Operations On Shell Ea Offshore</td>
 				<td>1997 -1998</td>
 				<td>Completed</td>
 			    </tr>
-			    <tr>
-				<td>CONOCO CEPNL-09-00</td>
-				<td>Provision of Rental Tools</td>
-				<td>2001-2002</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
-				<td>Stardeep Water Petroleum Ltd Contract LS00-009B</td>
-				<td>General Services Agreement for the supply of Downhole Equipment Rental and Rotating Mousehole Tool (RMT)TM and Twister Torque Tool (TT)TM used for breaking out and making up DCs, DPs.</td>
-				<td>2001-2002</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
-				<td>Halliburton Energy Services Contract Nig-SNEP-99-002</td>
-				<td>Rental &amp; Supply of Tubing Test String and Tubing Running Services </td>
-				<td>1999-2003</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
-				<td>Halliburton Energy Services Contract Ref. LOS-00-03-01</td>
-				<td>Integrated Completion Services Bonga Field</td>
-				<td>Aug 2002 - Aug 2005</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
-				<td>Nigeria Agip Exploration Limited Ref.230039/0</td>
-				<td>Provision of Down Hole Equipment Services</td>
-				<td>2004/2005</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
-				<td>Independent Energy Ref: 20.M0012.00/11001</td>
-				<td>Hydraulic Workover Operations</td>
-				<td>June 2007 to Sept 2007</td>
-				<td>Completed</td>
-			    </tr>
-			    <tr>
-				<td>Frontier Oil Ltd</td>
-				<td>Hire of Hydraulic Workover Unit [HWU] for Workover Ops Services.</td>
-				<td>Jan. 2008 to March 2008. Yet to commence.</td>
-				<td>Ongoing</td>
-			    </tr>
-			    <tr>
-				<td>ENI Congo Contract no. 8090000000/105</td>
-				<td>Provision of hydraulic workover for offshore/onshore workover operations.</td>
-				<td>Feb 2009 - till date</td>
-				<td>Ongoing</td>
-			    </tr>
-			    <tr>
-				<td>NAOC</td>
-				<td>Supply of snubbing rig(ISS 460K Hydrastar II',) for workover intervention in NAOC land area</td>
-				<td>June 2009 - June 2010</td>
-				<td>Ongoing</td>
-			    </tr>
+			    
 			</tbody>
 		    </table><br/>
 		</div>
