d4b4e983f7557572fb0014cc4e0a20ff3c04a92d
Oladayo Osikoya <alwaysdeone@gmail.com>
2010-01-06 07:59:55 +0100

    Added more capabilities. Warri and PH address added to contact panel.

--- capabilities.inc.php
+++ capabilities.inc.php
@@ -1,12 +1,16 @@
 <div class="right_column">
     <h2 id="h2-ourcapabilities"><span>Our Capabilities</span></h2>
     <ul>
-	<li>Casing &amp; Tubing Running</li>
-	<li>Downhole &amp; Fishing Tools Rentals &amp; Services</li>
+	<li>Downhole Drilling</li>
+	<li>Rental &amp; Fishing Tools</li>
 	<li>Drillstring &amp; Workstring Rentals</li>
+	<li>Casing &amp; Tubing Running Services</li>
+	<li>Rig Floor Equipment Supply &amp; Services</li>
+	<li>Well Intervention Services</li>
 	<li>Hydraulic Workover / Snubbing Services</li>
+	<li>Downhole &amp; Fishing Tools Rentals &amp; Services</li>
 	<li>Jar Servicing, Breakout &amp; Testing</li>
-	<li>Machineshop &amp; Fabrication</li>
-	<li>Rental Swamp Barges</li>
+	<li>Machineshop &amp; Fabrication Services</li>
+	<li>Logistics &amp; Consultancy</li>
     </ul>
 </div>
--- contact_panel.inc.php
+++ contact_panel.inc.php
@@ -7,5 +7,24 @@
 	47, Yeshayahu Lasode Crescent,<br/>
 	(By Mobil House), <abbr title="Victoria Island">VI</abbr>, Lagos<br/>
 	Nigeria.<br/><br/>
+
+	+234 (0)803 316 2577<br/>
+	<a href="mailto:esosaobaseki@teconoilserv.com" class="email">esosaobaseki@teconoilserv.com</a><br/><br/>
+
+	<strong>PORT HARCOURT</strong><br/>
+	Plot 41, Trans Amadi Layout,<br/>
+	Port Harcourt, Rivers State,<br/>
+	Nigeria.<br/><br/>
+
+	+234 (0)803 316 2597<br/>
+	<a href="mailto:johnnymaduafokwa@teconoilserv.com" class="email">johnnymaduafokwa@teconoilserv.com</a><br/><br/>
+
+	<strong>WARRI</strong><br/>
+	69, Airport Road, Ugborikoko,<br/>
+	Warri, Delta State,<br/>
+	Nigeria.<br/><br/>
+
+	+234 (0)803 407 1217<br/>
+	<a href="mailto:emekaobiejemba@teconoilserv.com" class=email">emekaobiejemba@teconoilserv.com</a>
     </address>
 </div>
